a404341d382cd40b59771d2ba8dc76847332d446
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-10-31 01:45:20 +0000

    Update mailbox.sql

--- mailbox.sql
+++ mailbox.sql
@@ -1,13 +1,13 @@
-CREATE TABLE `mailbox_mails` (
-   `id` int NOT NULL AUTO_INCREMENT,
-   `sender_id` varchar(50) references `users`(`identifier`),
-   `sender_firstname` varchar(50),
-   `sender_lastname` varchar(50),
-   `receiver_id` varchar(50) references `users`(`identifier`),
-   `receiver_firstname` varchar(50),
-   `receiver_lastname` varchar(50),
-   `message` text,
-   `opened` tinyint DEFAULT 0,
-   `received_at` datetime DEFAULT now(),
-   PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS `mailbox_mails` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sender_id` varchar(50) DEFAULT NULL,
+  `sender_firstname` varchar(50) DEFAULT NULL,
+  `sender_lastname` varchar(50) DEFAULT NULL,
+  `receiver_id` varchar(50) DEFAULT NULL,
+  `receiver_firstname` varchar(50) DEFAULT NULL,
+  `receiver_lastname` varchar(50) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `opened` tinyint(1) DEFAULT 0,
+  `received_at` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
